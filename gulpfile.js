d628f323a170d7b21a2f10b733c801622a713e45
Zerlingo <zerlingo88@gmail.com>
2021-10-14 23:43:12 +0300

    chore: setup autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const { watch, src, dest } = require('gulp');
 const sassModule = require('sass');
 const gulpSass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require("gulp-clean-css");
 
 const sass = gulpSass(sassModule);
 
 function buildStyles() {
   return src('./assets/stylesheet/index.scss')
     .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(
+        clean({
+          level: 2,
+        })
+    )
     .pipe(dest('./dist/stylesheet'));
 }
 
